1b5402f600f86f227c7f7ebbddcb8a6295009466
lufre1 <155526548+lufre1@users.noreply.github.com>
2024-05-13 17:27:15 +0200

    Update version_getter.py

--- deployment/version_getter.py
+++ deployment/version_getter.py
@@ -10,9 +10,9 @@ current_os = os.environ.get('RUNNER_OS').lower()  # Ensure lowercase for compari
 
 # Construct the filename based on OS name (consider all possibilities)
 if current_os == 'windows':
-    construct_file = "construct_windows-latest.yaml"
+    construct_file = os.path.join("construct_windows-latest.yaml")
 elif current_os == 'linux':
-    construct_file = "construct_ubuntu-latest.yaml"  # Assuming ubuntu-latest for Linux
+    construct_file = os.path.join("construct_ubuntu-latest.yaml")  # Assuming ubuntu-latest for Linux
 # Add an else block if you plan to support macOS in the future
 else:
     raise Exception(f"Unsupported OS: {current_os}")
@@ -26,5 +26,5 @@ with open(construct_file, "r") as stream:
 
 ctor_conf["version"] = runpy.run_path(os.path.join("..", "micro_sam", "__version__.py"))["__version__"]
 
-with open(ctor_fname, "w") as outfile:
+with open(construct_file, "w") as outfile:
     yaml.dump(ctor_conf, outfile)
