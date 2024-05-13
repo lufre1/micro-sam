e0213468f1f763dda13b1d6152760c0034cde8a7
lufre1 <155526548+lufre1@users.noreply.github.com>
2024-05-13 16:53:19 +0200

    Update version_getter.py

--- deployment/version_getter.py
+++ deployment/version_getter.py
@@ -4,9 +4,15 @@ import os
 
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
-ctor_fname = os.path.join("construct.yaml")
 
-with open(ctor_fname, "r") as stream:
+# Construct the filename based on OS name
+construct_file = f"construct_{current_os}.yaml"
+
+# Load YAML using ruamel.yaml
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
+with open(construct_file, "r") as stream:
     ctor_conf = yaml.load(stream)
 
 ctor_conf["version"] = runpy.run_path(os.path.join("..", "micro_sam", "__version__.py"))["__version__"]
